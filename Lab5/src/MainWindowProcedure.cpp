6311824602f328824ebb2ef7ac2f3251b99c7af3
Inaceat <10551818+Inaceat@users.noreply.github.com>
2019-03-20 21:59:25 +0300

    Drawing prepared.

--- Lab5/src/MainWindowProcedure.cpp
+++ Lab5/src/MainWindowProcedure.cpp
@@ -17,6 +17,26 @@ BOOL GetClientSize(HWND hWnd, int& width, int& height)
 }
 
 
+void DrawAllTheShit(HWND* hWnd, HDC* dc)
+{
+	//Get drawing area size
+	int windowWidth;
+	int windowHeight;
+
+	GetClientSize(*hWnd, windowWidth, windowHeight);
+
+	//Fill it with white
+	RECT rect = { 0, 0, windowWidth + 1, windowHeight + 1 };
+	FillRect(*dc, &rect, (HBRUSH)(COLOR_WINDOW + 1));
+	
+
+	//Draw things
+	
+}
+
+
+
+
 INT_PTR CALLBACK MainWindowProcedure(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	static int windowWidth;
@@ -25,39 +45,43 @@ INT_PTR CALLBACK MainWindowProcedure(HWND hWnd, UINT message, WPARAM wParam, LPA
 	static HDC windowDC;
 	static HDC bufferDC;
 
-
+	
 	switch (message)
 	{
-		case WM_CREATE:
-			//Get window size
-			GetClientSize(hWnd, windowWidth, windowHeight);
+	case WM_INITDIALOG:
+		//Get window size
+		GetClientSize(hWnd, windowWidth, windowHeight);
 
-			//Create memory DC for drawing
-			windowDC = GetDC(hWnd);
-			
-			bufferDC = CreateCompatibleDC(windowDC);
+		//Create memory DC for drawing
+		windowDC = GetDC(hWnd);
+		bufferDC = CreateCompatibleDC(windowDC);
+		
+		//Create bitmap and insert to buffer DC
+		DeleteObject(SelectObject(bufferDC, CreateCompatibleBitmap(windowDC, windowWidth, windowHeight)));
 
-			//Create bitmap and insert to buffer DC
-			DeleteObject(SelectObject(bufferDC, CreateCompatibleBitmap(windowDC, windowWidth, windowHeight)));
+		DrawAllTheShit(&hWnd, &bufferDC);
 
-			break;
-
-
-		case WM_PAINT:
-
-			windowDC = GetDC(hWnd);
-			BitBlt(windowDC	, 0, 0, windowWidth, windowHeight, bufferDC, 0, 0, SRCCOPY);
-
-			break;
+		break;
 
 
-		case WM_CLOSE:
-			PostQuitMessage(42);
-			break;
+	case WM_PAINT:
+		//Paste from buffer DC to screen
+		windowDC = GetDC(hWnd);
+
+		BitBlt(windowDC, 0, 0, windowWidth, windowHeight, bufferDC, 0, 0, SRCCOPY);
+
+		ReleaseDC(hWnd, windowDC);
+
+		break;
 
 
-		default:
-			return FALSE;
+	case WM_CLOSE:
+		PostQuitMessage(42);
+		break;
+
+
+	default:
+		return FALSE;
 	}
 
 	return TRUE;
